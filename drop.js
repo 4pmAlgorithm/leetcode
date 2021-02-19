0ff6c37cfb00e20f52a32a81a0352cf6f8e99097
labradorescence <orangenewyorkcreativeagency@gmail.com>
2021-02-19 08:51:17 -0500

    adding wordList set iteration

--- drop.js
+++ drop.js
@@ -6,22 +6,33 @@ function solution (wordList, keypads){
     for(let i = 0; i < keypads.length; i++){ //keypadspad outer loop  
       let keypadsSet = new Set(keypads[i]) 
       console.log(keypadsSet)
+
       let setHasChar = false 
 
       for(let j = 0; j < wordList.length; j++){//inner loop wordList loop 
-        
-          for(let k = 0; k < wordList[j].length; k++){ //each wordList char
-            if(keypadsSet.has(wordList[j][k])){
-              console.log(wordList[j][k])
-              setHasChar = true
-            } else {
-              setHasChar = false
-              break;
-            }
+        let wordListSet = new Set(wordList[j]) //each wordList char
+        console.log("w", wordListSet)  
+
+        function eachWordChar(wordListSet){
+           console.log("::", wordListSet)
+        }
+
+        wordListSet.forEach(wordListSet => wordListSet)
+
+        console.log("??", wordListSet.forEach(eachWordChar))
+
+            // if(keypadsSet.has(wordListSet.forEach(eachWordChar))){
+            //   console.log("???", wordListSet.forEach(eachWordChar))
+            //   setHasChar = true
+            // } else {
+            //   setHasChar = false
+            //   break;
+            // }
+
+
             if(wordList[j].includes(keypads[i][0]) && setHasChar && k === wordList[j].length-1 ){
               count += 1      
             }
-          } 
       }//end of inner loop wordList loop
       result.push(count)
       count = 0    
