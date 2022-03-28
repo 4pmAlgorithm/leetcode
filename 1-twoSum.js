8ec8c5d5e23f5bdb575aeb4393e16206033a97f2
Greem Jellyfish <orangenewyorkcreativeagency@gmail.com>
2022-03-28 17:27:18 -0400

    two sum

--- 1-twoSum.js
+++ 1-twoSum.js
@@ -1,9 +1,52 @@
 // https://leetcode.com/problems/two-sum/
 
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+//5 03/28/2022 4:55pm - 5:15 gave up and looked at answer
 //4 March/09/2022 5:34PM - 5:41 couldn't solve it so looked at solution
 //3
 //2
 //1
+
+
+function twoSum(arr, target){
+    //1. obj { ele: index }
+    //2. for loop
+    //3. sub = target - ele 
+    //4. if sub exist in the obj as a key, then return index of that sub, and return ele index
+
+    let obj = {}
+
+    for(let i = 0; i < arr.length; i++){
+        obj[arr[i]] = i;
+    }
+
+    for(let i =0; i <arr.length; i++){
+        let sub = target - arr[i];
+
+        if( obj[sub] !== undefined && obj[sub] !== i  ){
+            console.log([i, obj[sub]])
+            return [i, obj[sub]]
+        }
+    }
+}
+
+twoSum([3,2,4], 6)
+// [
+// 3, 6-3=3
+// 2, 6-2=4
+// 4  6-4=2
+// ], 
+// 6
 
 
 // [
@@ -14,29 +57,29 @@
 
 // 6
 
-function twoSum (arr, target){
-    let obj = {}
-    if (arr.length === 2) return [0, 1];
+// function twoSum (arr, target){
+//     let obj = {}
+//     if (arr.length === 2) return [0, 1];
 
-    for(let i = 0; i < arr.length; i ++){
-        obj[arr[i]] = i;
-    }
+//     for(let i = 0; i < arr.length; i ++){
+//         obj[arr[i]] = i;
+//     }
 
-    for(let i = 0; i < arr.length; i++) {
-        let complement = target - arr[i]; 
-        console.log(`${complement} = ${target} - ${arr[i]}`)
-        let found = obj[complement]; // Determine whether the complement exist in the hashTable
-        console.log("FOUND::::  ", found)
-        console.log(obj)
+//     for(let i = 0; i < arr.length; i++) {
+//         let complement = target - arr[i]; 
+//         console.log(`${complement} = ${target} - ${arr[i]}`)
+//         let found = obj[complement]; // Determine whether the complement exist in the hashTable
+//         console.log("FOUND::::  ", found)
+//         console.log(obj)
 
-        if(found !== undefined && found != i){
-            console.log([i, found]) 
-            return [i, found];
-        }
-	}
-    console.log([0,0])
-	return [0,0];
-}
+//         if(found !== undefined && found != i){
+//             console.log([i, found]) 
+//             return [i, found];
+//         }
+// 	}
+//     console.log([0,0])
+// 	return [0,0];
+// }
 
 // /**
 //  * @param {number[]} nums
@@ -90,5 +133,5 @@ function twoSum (arr, target){
 // }
 
 
-twoSum([3,2,3], 6)
+//twoSum([3,2,3], 6)
 
--- 1365-howmany.js
+++ 1365-howmany.js
@@ -1,52 +1,97 @@
+// Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+// Return the answer in an array.
+
+// Input: nums = [8,1,2,2,3]
+// Output: [4,0,1,1,3]
+// Explanation: 
+// For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+// For nums[1]=1 does not exist any smaller number than it.
+// For nums[2]=2 there exist one smaller number than it (1). 
+// For nums[3]=2 there exist one smaller number than it (1). 
+// For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+//#3 2022/03/15 1:12pm - 
 //#2 2022/03/10 #1365 howmany 5:15pm- 5:24pm
 //#1 2022/03/10 #1365 howmany 4:15pm- 5:15pm
 
 
-//#2 2022/03/10 #1365 howmany 5:15pm- 5:24pm
-var smallerNumbersThanCurrent = function(nums) {
-   
- let sorted = [...nums].sort((a,b) => a-b)
- let hash = {}, result = [];
-  
-  for(let i = 0; i < sorted.length; i++){
-      if(sorted[i] === sorted[i-1]) continue;
-      hash[sorted[i]] = i
-  }
-  
-  nums.forEach((each, i) => {
-      result[i] = hash[each]
-  })
-  
- //  console.log(result)
- // console.log(hash)
-  return result
-};
+// 1. sort the arr 
+// 2. store the sorted num in obj w index num as val
+// 3. skip the same num 
+// 4. return the index num for each ele of the original arr 
 
 
-
-
-//#1 2022/03/10 #1365 howmany 4:15pm- 5:15pm
 
 function smallerNumbersThanCurrent(nums){
- let sorted = [...nums].sort((a,b) => a-b) //concat the nums in an arr
+    let sorted = [...nums].sort((a,b) => a-b)
 
- let hash = {}, result = [];
+    let hash = {}, result = []
 
- for(let i = 0; i < sorted.length; i++){
-  console.log(sorted[i], sorted[i-1])
-  if(sorted[i] === sorted[i-1]) continue; //if the ele are same, then skip
-  hash[sorted[i]] = i
- }
+    for(let i = 0; i < sorted.length; i++){
+        if(sorted[i] === sorted[i-1]) continue;
+        hash[sorted[i]] = i
+    }
 
+    //console.log(hash)
 
-
- for(let i = 0; i < nums.length; i++){
-  result[i] = hash[nums[i]]
- }
- console.log(nums)
- console.log(result)
- console.log(hash)
+    for(let i = 0; i < nums.length; i++){
+        result.push(hash[nums[i]])     
+    }
+    console.log(result)
+    return result
 }
+
+
+
+smallerNumbersThanCurrent([8,1,2,2,3])
+
+
+//#2 2022/03/10 #1365 howmany 5:15pm- 5:24pm
+// var smallerNumbersThanCurrent = function(nums) {
+   
+//  let sorted = [...nums].sort((a,b) => a-b)
+//  let hash = {}, result = [];
+  
+//   for(let i = 0; i < sorted.length; i++){
+//       if(sorted[i] === sorted[i-1]) continue;
+//       hash[sorted[i]] = i
+//   }
+  
+//   nums.forEach((each, i) => {
+//       result[i] = hash[each]
+//   })
+  
+//  //  console.log(result)
+//  // console.log(hash)
+//   return result
+// };
+
+
+
+
+//#1 2022/03/10 #1365 howmany 4:15pm- 5:15pm
+
+// function smallerNumbersThanCurrent(nums){
+//  let sorted = [...nums].sort((a,b) => a-b) //concat the nums in an arr
+
+//  let hash = {}, result = [];
+
+//  for(let i = 0; i < sorted.length; i++){
+//   console.log(sorted[i], sorted[i-1])
+//   if(sorted[i] === sorted[i-1]) continue; //if the ele are same, then skip
+//   hash[sorted[i]] = i
+//  }
+
+
+
+//  for(let i = 0; i < nums.length; i++){
+//   result[i] = hash[nums[i]]
+//  }
+//  console.log(nums)
+//  console.log(result)
+//  console.log(hash)
+// }
 
 
 
@@ -94,7 +139,7 @@ function smallerNumbersThanCurrent(nums){
 // return res
 // }
 
-smallerNumbersThanCurrent([8,1,2,2,3])
+// smallerNumbersThanCurrent([8,1,2,2,3])
 
 //#1 not passing, time out  
 // var smallerNumbersThanCurrent = function(nums) {
